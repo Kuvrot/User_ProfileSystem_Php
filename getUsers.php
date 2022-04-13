1507c7bd4000cca64bb94a89118a82a38322b9bb
Kuvrot <jamessanz@outlook.com>
2022-04-13 13:05:38 -0500

    ahora se usa ajax para decirle al usuario si la contraseña es incorrecta

--- getUsers.php
+++ getUsers.php
@@ -10,9 +10,15 @@ while ($row = $stmt->fetch()) {
 
     $name = $row['m_username'];
 
+
+    if ($session_active){
+       ?> <h2 class="w-25">You are logged as <?php echo $current_user;?> </h2>
+   <?php }{
+       ?> <h2 class="w-25">You are not logged yet <?php echo $current_user;?> </h2> <?php
+   }
+
    ?>
-    <h2 class="w-25">You are logged as <?php echo $name?> </h2>
-        
+
     <a href="viewProfile.php?user=<?php echo $name ?>" 
         class="list-group-item list-group-item-action btn w-25 center-content"> 
            <?php  echo $name?></a>
